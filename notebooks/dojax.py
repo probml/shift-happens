597f84bdad979f508d73c5b98ba6219103f2a82c
Gerardo Durán-Martín <g.duran@me.com>
2022-02-23 21:40:54 +0000

    feat: dojax.py

--- notebooks/dojax.py
+++ notebooks/dojax.py
@@ -2,6 +2,8 @@
 import jax
 import numpy as np
 import jax.numpy as jnp
+from multiprocessing import Pool
+from augly import image
 
 
 def rotation_matrix(angle):
@@ -16,25 +18,25 @@ def rotation_matrix(angle):
     return R
 
 
-  def flat_and_concat_params(params_hist):
-    """
-    Flat and concat a list of parameters trained using
-    a Flax model
-    
-    
-    Parameters
-    ----------
-    params_hist: list of flax FrozenDicts
-        List of flax FrozenDicts containing trained model
-        weights.
-    
-    Returns
-    -------
-    jnp.array: flattened and concatenated weights
-    """
-    flat_params = [jax.flatten_util.ravel_pytree(params)[0] for params in params_hist]
-    flat_params  = jnp.r_[flat_params]
-    return flat_params
+def flat_and_concat_params(params_hist):
+  """
+  Flat and concat a list of parameters trained using
+  a Flax model
+
+
+  Parameters
+  ----------
+  params_hist: list of flax FrozenDicts
+      List of flax FrozenDicts containing trained model
+      weights.
+
+  Returns
+  -------
+  jnp.array: flattened and concatenated weights
+  """
+  flat_params = [jax.flatten_util.ravel_pytree(params)[0] for params in params_hist]
+  flat_params  = jnp.r_[flat_params]
+  return flat_params
 
 
 def make_mse_func(model, x_batched, y_batched):
@@ -47,3 +49,74 @@ def make_mse_func(model, x_batched, y_batched):
     # We vectorize the previous to compute the average of the loss on all samples.
     return jnp.mean(jax.vmap(squared_error)(x_batched, y_batched), axis=0)
   return jax.jit(mse)
+
+
+### Elements for shifting-mnist experiments ###
+
+class BlurRad:
+    def __init__(self, rad):
+        self.rad = rad
+        
+    def __call__(self, img):
+        return self.blur_multiple(img)
+
+    def blur(self, X):
+        """
+        Blur an image using the augly library
+
+        Paramters
+        ---------
+        X: np.array
+            A single NxM-dimensional array
+        radius: float
+            The amout of blurriness
+        """
+        return image.aug_np_wrapper(X, image.blur, radius=self.rad)
+
+    def blur_multiple(self, X_batch):
+        images_out = []
+        for X in X_batch:
+            img_blur = self.blur(X)
+            images_out.append(img_blur)
+        images_out = np.stack(images_out, axis=0)
+        return images_out
+
+
+def blur_multiple(radii, img_dataset):
+    """
+    Blur every element of `img_dataset` given an element
+    of `radii`.
+    """
+    imgs_out = []
+    for radius, img in zip(radii, img_dataset):
+        img_proc = BlurRad(radius).blur(img)
+        imgs_out.append(img_proc)
+    imgs_out = jnp.stack(imgs_out, axis=0)
+    
+    return imgs_out
+
+
+def proc_dataset_multiple(radii, img_dataset, n_processes=90):
+    """
+    Blur all images of a dataset stored in a numpy array with variable
+    radius.
+    
+    Parameters
+    ----------
+    radius: array(N,)
+        Intensity of bluriness. One per dataset
+    img_dataset: array(N, L, K)
+        N images of size LxK
+    n_processes: int
+        Number of processes to blur over
+    """
+
+    with Pool(processes=n_processes) as pool:
+        dataset_proc = np.array_split(img_dataset, n_processes)
+        radii_split = np.array_split(radii, n_processes)
+        
+        elements = zip(radii_split, dataset_proc)
+        dataset_proc = pool.starmap(blur_multiple, elements)
+        dataset_proc = np.concatenate(dataset_proc, axis=0)
+
+    return dataset_proc
