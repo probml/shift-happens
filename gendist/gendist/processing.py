5e1a24b9a149db35aef1eeba862655fafb3fc03d
Gerardo Durán-Martín <g.duran@me.com>
2022-03-07 08:57:25 +0000

    feat: processing.py
    Define Factory class for model training

--- gendist/gendist/processing.py
+++ gendist/gendist/processing.py
@@ -1,64 +1,80 @@
 """
 This library contains functions to process image data used by GenDist
 """
+import jax
+import numpy as np
+import jax.numpy as jnp
+from multiprocessing import Pool
+from augly import image 
 
-class BlurRad:        
-    def __call__(self, img, radius):
-        return self.blur_multiple_multiprocessing(img, radius)
+class Factory:
+    """
+    This is a base library to process / transform the elements of a numpy
+    array according to a given function. To be used with gendist.TrainingConfig
+    """
+    def __init__(self, processor):
+        self.processor = processor
+    
+    def __call__(self, img, **kwargs):
+        return self.process_multiple_multiprocessing(img, kwargs)
 
-    def blur(self, X, radius):
+    def process_single(self, X, **kwargs):
         """
-        Blur an image using the augly library
+        Process a single element.
 
         Paramters
         ---------
         X: np.array
-            A single NxM-dimensional array
-        radius: float
-            The amout of blurriness
+            A single numpy array
+        kwargs: dict/params
+            Processor's configuration parameters
         """
-        return image.aug_np_wrapper(X, image.blur, radius=radius)
+        return self.processor(X, **kwargs)
 
-    def blur_multiple(self, X_batch, radii):
-        images_out = []
+    def process_multiple(self, X_batch, configurations):
+        """
+        Process all elements of a numpy array according to a list
+        of configurations.
+        Each image is processed according to a configuration.
+        """
+        X_out = []
         n_elements = len(X_batch)
                     
-        for X, radius in zip(X_batch, radii):
-            img_blur = self.blur(X, radius)
-            images_out.append(img_blur)
+        for X, configuration in zip(X_batch, configurations):
+            X_processed = self.process_single(X, **configuration)
+            X_out.append(X_processed)
             
-        images_out = np.stack(images_out, axis=0)
-        return images_out
+        X_out = np.stack(X_out, axis=0)
+        return X_out
     
-    def blur_multiple_multiprocessing(self, X_dataset, radii, n_processes=90):
+    def process_multiple_multiprocessing(self, X_dataset, configurations, n_processes=90):
         """
-        Blur all images of a dataset stored in a numpy array with variable
-        radius.
+        Process elements in a numpy array in parallel.
 
         Parameters
         ----------
-        radius: array(N,) or float
-            Intensity of bluriness. One per image. If
-            float, the same value is used for all images.
-        img_dataset: array(N, L, K)
-            N images of size LxK
+        X_dataset: array(N, ...)
+            N elements of arbitrary shape
+        configurations: list
+            List of configurations to apply to each element. Each
+            element is a dict to pass to the processor.
         n_processes: int
-            Number of processes to blur over
+            Number of cores to use
         """
         num_elements = len(X_dataset)
-        if type(radii) in [float, np.float_]:
-            radii = radii * np.ones(num_elements)
+        if type(configurations) == dict:
+            configurations = [configurations] * num_elements
 
         dataset_proc = np.array_split(X_dataset, n_processes)
-        radii_split = np.array_split(radii, n_processes)
-        elements = zip(dataset_proc, radii_split)
+        config_split = np.array_split(configurations, n_processes)
+        elements = zip(dataset_proc, config_split)
 
         with Pool(processes=n_processes) as pool:    
-            dataset_proc = pool.starmap(self.blur_multiple, elements)
+            dataset_proc = pool.starmap(self.process_multiple, elements)
             dataset_proc = np.concatenate(dataset_proc, axis=0)
 
         return dataset_proc.reshape(num_elements, -1)
-    
+
 
 def flat_and_concat_params(params_hist):
     """
