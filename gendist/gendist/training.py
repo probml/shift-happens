f56af19af3cd1303ecadff6ed2973bad22b67974
Gerardo Durán-Martín <g.duran@me.com>
2022-03-23 09:17:21 +0000

    brushoff: training.py
    Remove TrainingShift definition

--- gendist/gendist/training.py
+++ gendist/gendist/training.py
@@ -257,7 +257,6 @@ class TrainingMeta(TrainingBase):
 
         losses = []
         for e in tqdm(range(num_epochs), leave=leave_pb):
-            print(f"@epoch {e+1:03}", end="\r")
             _, key = jax.random.split(key)
             params, optimiser_state, avg_loss = self.train_epoch(key, params, optimiser_state,
                                                                  X_train, y_train, batch_size, e)
@@ -294,98 +293,3 @@ class TrainingMeta(TrainingBase):
         
         epoch_loss = epoch_loss / len(batch_ixs)
         return params, opt_step, epoch_loss
-
-
-class TrainingShift:
-    def __init__(self, model, processor, loss_generator, tx):
-        """
-        Class to train a neural network a dataset corrupted by "shifts"
-
-        Parameters
-        ----------
-        model: flax.nn.Module
-            Model to train. It takes as inputs an instance of the dataset
-            and tries to estimate the weights of the input model.
-        processor: function
-            Function that takes a single sample and returns a transformed sample.
-        loss_generator: function
-            Loss function generator for the model.
-        tx: optax.Optimiser
-            Optimiser to use for training.
-        """
-        self.model = model
-        self.processor = processor
-        self.loss_generator = loss_generator
-        self.tx = tx
-
-    def get_batch_train_configs_ixs(self, num_samples, num_configs, batch_size):
-        """
-        Obtain the training indices to be used in an epoch of
-        mini-batch optimisation
-        
-        We consider a tranining dataset over number of samples and
-        number of configurtios.
-        """
-        num_elements = num_samples * num_configs
-        
-        steps_per_epoch = num_elements // batch_size
-        
-        batch_ixs = np.random.permutation(num_elements)
-        batch_ixs = batch_ixs[:steps_per_epoch * batch_size]
-        batch_ixs = batch_ixs.reshape(steps_per_epoch, batch_size)
-        
-        batch_ixs_samples = batch_ixs // num_configs
-        batch_ixs_configs = batch_ixs % num_configs
-        
-        return batch_ixs_samples, batch_ixs_configs
-
-    @partial(jax.jit, static_argnums=(0,))
-    def train_step(self, params, opt_state, X_batch, y_batch):
-        loss_fn = self.loss_generator(self.model, X_batch, y_batch)
-        loss_grad_fn = jax.value_and_grad(loss_fn)
-        
-        loss_val, grads = loss_grad_fn(params)
-        updates, opt_state = self.tx.update(grads, opt_state)
-        params = optax.apply_updates(params, updates)
-        
-        return loss_val, params
-    
-    
-    def train_epoch(self, key, params, opt_step, X, y, configs, batch_size, epoch, logger, n_processes=90):
-        """
-        Training epoch for the projected weights. Note that target weights are the
-        target variables
-        """
-        num_samples = len(X)
-        num_configs = len(configs)
-        n_processes_batch = min(n_processes, batch_size)
-        
-        sample_ixs, config_ixs = self.get_batch_train_configs_ixs(num_samples, num_configs, batch_size)
-        
-        number_partitions = len(sample_ixs)
-        for part, (sample_batch_ix, config_batch_ix) in enumerate(zip(sample_ixs, config_ixs)):
-            print(f"{part+1:03}/{number_partitions}", end="\r")
-            X_batch = X[sample_batch_ix, ...]
-            config_batch = [configs[ix] for ix in config_batch_ix]
-            X_batch_transformed = self.processor(X_batch, config_batch, n_processes_batch)
-            
-            y_batch = y[config_batch_ix, ...]
-            loss, params = self.train_step(params, opt_step, X_batch_transformed, y_batch)
-
-        logger.info(f"@{epoch=} | {loss=:0.4f}")
-        return loss, params, opt_step
-
-    def train(self, key, X_train, target_values, configurations, batch_size, num_epochs, logger, params=None):
-        num_samples, *obs_size = X_train.shape
-        losses = []
-        key_init, key_updates = jax.random.split(key)
-        key_updates = jax.random.split(key_updates, num_epochs)
-
-        batch = jnp.ones((1, jnp.prod(jnp.asarray(obs_size))))
-        params = self.model.init(key, batch) if params is None else params
-        opt_state = self.tx.init(params)
-        for epoch, key in enumerate(key_updates):
-            loss, params, opt_state = self.train_epoch(key, params, opt_state, X_train, target_values,
-                                                    configurations, batch_size, epoch, logger)
-            losses.append(loss.item())
-        return params, losses
