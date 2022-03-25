664a240782a17b9a9b888bb5a0d65ea06c0c5826
Gerardo Durán-Martín <g.duran@me.com>
2022-03-25 18:30:19 +0000

    feat: gendist/models.py
    Add LeNet5

--- gendist/gendist/models.py
+++ gendist/gendist/models.py
@@ -1,4 +1,26 @@
 import flax.linen as nn
+from typing import Callable
+import jax.numpy as jnp
+
+class LeNet5(nn.Module):
+  num_classes : int
+  activation  : Callable[[jnp.ndarray], jnp.ndarray] = nn.relu
+  @nn.compact
+  def __call__(self, x):
+    """Aleyna's network inspired by LeNet-5."""
+    x = x if len(x.shape) > 1 else x[None, :]
+    x = x.reshape((x.shape[0], 28, 28, 1))
+    x = self.activation(nn.Conv(features=6, kernel_size=(5,5), padding="SAME")(x))
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+    x = self.activation(nn.Conv(features=16, kernel_size=(5,5), padding="VALID")(x))
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+    x = x.reshape((x.shape[0], -1))
+    x = self.activation(nn.Dense(features=120)(x))
+    x = self.activation(nn.Dense(features=84)(x))
+    x = nn.Dense(features=self.num_classes)(x)
+    x = nn.log_softmax(x)
+    return x
+
 
 class MLPDataV1(nn.Module):
     num_outputs: int
