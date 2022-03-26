bb8a352fe21f9ed65ec4e373af08cae7f4e60bc2
Gerardo Durán-Martín <g.duran@me.com>
2022-03-26 12:24:14 +0000

    refactor: mnist_rotation_meta
    Add helper functions

--- gendist/experiments/mnist_rotation_meta.py
+++ gendist/experiments/mnist_rotation_meta.py
@@ -9,7 +9,7 @@ from augly import image
 from sklearn.decomposition import PCA
 
 
-def processor(X, angle):
+def rotate(X, angle):
     X_shift = image.aug_np_wrapper(X, image.rotate, degrees=angle)
     size_im = X_shift.shape[0]
     size_pad = (28 - size_im) // 2
@@ -19,23 +19,95 @@ def processor(X, angle):
     return X_shift
 
 
+def load_train_combo(filename):
+    """
+    Load the parameters and the configurations from a file
+    of trained models. We return the flatten weights,
+    the reconstruct function and the configurations.
+    """
+    with open(filename, "rb") as f:
+        params = pickle.load(f)
+        list_params = params["params"]
+        list_configs = params["configs"]
+    
+    target_params, fn_reconstruct = gendist.processing.flat_and_concat_params(list_params)
+
+    output = {
+        "params": target_params,
+        "configs": list_configs,
+        "fn_reconstruct": fn_reconstruct
+    }
+
+    return output
+
+
+def configure_covariates(key, processor, X, configs, n_subset):
+    """
+    Given a dataset with shape (n_train, ...), a subset size n_subset,
+    and a list of configurations to transform the dataset, we transform the
+    dataset in an array of shape (n_subset, n_features, ...).
+    """
+    n_configs = len(configs)
+    n_train, *elem_dims = X.shape
+
+    imap = np.ones((n_configs, 1, *elem_dims))
+    configs_transform = np.repeat(configs, n_subset)
+    subset_ix = jax.random.choice(key, n_train, (n_subset,), replace=False).to_py()
+    X = X[subset_ix, ...] * imap
+    X = processor(X.reshape(-1, *elem_dims), configs_transform)
+    X = X.reshape((n_subset, n_configs, -1), order="F")
+
+    return X
+
+
+def predict_shifted_dataset(ix_seed, X_batch, processor, config, meta_model,
+                            meta_params, dmodel, proj, fn_reconstruct):
+    """
+    Predict weights and estimate the values
+    
+    Parameters
+    ----------
+    ix_seed: array
+    X_batch: array
+    ...
+    meta_model: model for the latent space
+    meta_params: trained weights for the latent space
+    dmodel: model for the observed space
+    dparams: trained model for the observed weights
+    """
+    x_seed = X_batch[ix_seed]
+    x_shift = processor.process_single(x_seed, **config).ravel()
+    predicted_weights = meta_model.apply(meta_params, x_shift)
+    predicted_weights = proj.inverse_transform(predicted_weights)
+    predicted_weights = fn_reconstruct(predicted_weights)
+    
+    X_batch_shift = processor(X_batch, config)
+    y_batch_hat = dmodel.apply(predicted_weights, X_batch_shift)
+    
+    return y_batch_hat
+
+
+processing_class = gendist.processing.Factory(rotate)
+
+
 if __name__ == "__main__":
     import sys
+
     _, filename_data_model = sys.argv
     experiment_path, _ = os.path.split(filename_data_model)
 
-    with open(filename_data_model, "rb") as f:
-        data_model_config = pickle.load(f)
-        list_params = data_model_config["params"]
-        configs = data_model_config["configs"]
+    output = load_train_combo(filename_data_model)
+    target_params = output["params"]
+    list_configs = output["configs"]
+    fn_reconstruct_params = output["fn_reconstruct"]
 
-    target_params, fn_recontruct_params = gendist.processing.flat_and_concat_params(list_params)
-    processing_class = gendist.processing.Factory(processor)
+    processing_class = gendist.processing.Factory(rotate)
     key = jax.random.PRNGKey(314)
     key, key_subset = jax.random.split(key)
 
     mnist_train = torchvision.datasets.MNIST(root=".", train=True, download=True)
     X_train = np.array(mnist_train.data) / 255
+    X_train = configure_covariates(key_subset, processing_class, X_train, list_configs, n_train_subset)
 
     n_components = 60
     n_classes = 10
@@ -46,15 +118,8 @@ if __name__ == "__main__":
     pca = PCA(n_components=n_components)
     projected_params = pca.fit_transform(target_params)[None, ...]
 
-    imap = np.ones((n_configs, 1, *elem_dims))
-    configs_transform = np.repeat(configs, n_train_subset)
-    subset_ix = jax.random.choice(key_subset, n_train, (n_train_subset,), replace=False).to_py()
-    X_train = X_train[subset_ix, ...] * imap
-    X_train = processing_class(X_train.reshape(-1, *elem_dims), configs_transform)
-    X_train = X_train.reshape((n_train_subset, n_configs, -1), order="F")
-
     alpha = 0.01
-    n_epochs = 100
+    n_epochs = 150
     batch_size = 2000
     tx = optax.adam(learning_rate=alpha)
     lossfn = gendist.training.make_multi_output_loss_func
